3c4dd37f5916c83e6d4a1fbd5c81adccc0521cb8
roschel <joaomroschel@gmail.com>
2022-06-24 10:29:05 -0300

    initial commit

---
+++ RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Services/Implementations/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person Update(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        void DeleteById(long id);
+    }
+}
---
+++ RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,50 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void DeleteById(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>(); 
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i+1);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+
+        public Person FindById(long id)
+        {
+            return new Person { Id = IncrementAndGet(), FirstName = "João", LastName = "Roschel", Gender = "Male", Address = "Rua xxx" };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person { Id = IncrementAndGet(), FirstName = "João" + i, LastName = "Roschel", Gender = "Male", Address = "Rua xxx" }; 
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
